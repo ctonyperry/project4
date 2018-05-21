ae1b1c24a1d6575c6b94564692b3b3507a55f401
Tony Perry <ctonyperry@gmail.com>
2018-05-21 13:31:55 -0700

    add 'active' css style to activated navbar linbk

--- users-ui/src/app/navbar/navbar.component.ts
+++ users-ui/src/app/navbar/navbar.component.ts
@@ -1,15 +1,37 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
 
+  isHomeActive : any;
+  isAddUserActive: any;
   constructor() { }
 
   ngOnInit() {
+    this.isHomeActive='active';
+  }
+
+  activateLink(linkName){
+    switch(linkName){
+      case "home":
+      this.isHomeActive = 'active';
+      this.isAddUserActive = '';
+
+      break;
+
+      case "adduser":
+      this.isHomeActive = '';
+      this.isAddUserActive = 'active';
+
+      break;
+    }
+    
+   
   }
 
 }
