4a77432a9d8216ae560a4b6bc762309bde599930
Tony Perry <ctonyperry@gmail.com>
2018-06-01 10:26:05 -0700

    update production api host

--- ticket-notify/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiHost: "http://ec2-52-53-238-11.us-west-1.compute.amazonaws.com:8080"
-};
---
+++ ui/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiHost: "http://ec2-52-53-129-170.us-west-1.compute.amazonaws.com:8080"
+};
